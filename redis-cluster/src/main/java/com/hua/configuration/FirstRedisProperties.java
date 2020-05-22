367a304cd4244baa3fb7f4bd122d1cfb91d79c2a
qianye.zheng <qianye.zheng@wehotelglobal.com>
2020-05-22 19:00:57 +0800

    no message

---
+++ redis-cluster/src/main/java/com/hua/configuration/FirstRedisProperties.java
@@ -0,0 +1,55 @@
+/**
+  * @filename FirstRedisProperties.java
+  * @description 
+  * @version 1.0
+  * @author qianye.zheng
+ */
+package com.hua.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+/**
+ * @type FirstRedisProperties
+ * @description 
+ * @author qianye.zheng
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "spring.redis")
+public class FirstRedisProperties
+{
+
+	private String host;
+	
+	private String password;
+	
+	private Integer database;
+	
+	/* 端口 */
+	private Integer port;
+	
+	/* 连接超时时间，单位: 毫秒 */
+	private Long connectTimeout;
+	
+	/* 读超时时间，单位: 毫秒 */
+	private Long readTimeout;
+	
+	/** 连接池配置(Jedis) */
+	/* 最小空闲数 */
+	private Integer minIdle;
+	
+	/* 最大空闲数 */
+	private Integer maxIdle;
+	
+	/* 最大连接数，单位: 毫秒 */
+	private Integer maxTotal;
+	
+	/* 创建时是否测试可用 */
+	private Boolean testOnCreate;
+	
+	/* 最大等待时间，单位: 毫秒 */
+	private Long maxWaitMillis;
+}
